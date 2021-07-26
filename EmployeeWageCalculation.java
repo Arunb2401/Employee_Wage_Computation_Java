23fc9ad4222a74e30de37f23ef4c5f9e4f073263
Arunb2401 <arunbiradar24@gmail.com>
2021-07-26 14:16:32 +0530

    Added: Welcome to Employee Wage Computation

---
+++ EmployeeWageCalculation.java
@@ -0,0 +1,11 @@
+package com.employewage;
+
+public class EmployeeWageCalculation {
+	
+		public static void main(String[] args) 
+{
+		System.out.println("Welcome to Employee Wage Computation");
+
+		}
+	
+}
