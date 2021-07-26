38917eed39ba4c28fa78dbdd97e8bbdea07b689d
Arunb2401 <arunbiradar24@gmail.com>
2021-07-26 15:00:31 +0530

    Added : Employee is Present or Absent

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -1,11 +1,20 @@
 package com.employewage;
 
-public class EmployeeWageCalculation {
-	
-		public static void main(String[] args) 
+public class EmployeeWageCalculation 
 {
-		System.out.println("Welcome to Employee Wage Computation");
-
-		}
+	
+		 public static void main(String[] args) 
+	{
+		    	System.out.println(" Welcome to Employee Wage Computation ");
+		    	System.err.println(" Let's first check  whether Employee is Present or Absent: -  ");
+			
+		    	int employee = 1;
+		    	double empcheck = Math.floor( Math.random() *10 ) % 2;
+			
+		    	if ( empcheck == employee )
+		    		System.out.println(" Employee is Present Today ");
+		    	else
+		    		System.out.println(" Employee is Absent Today ");
+	}
 	
 }
