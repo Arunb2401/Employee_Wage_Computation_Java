a0f428bd0f907396078806e77fa3ed0703aac582
Arunb2401 <arunbiradar24@gmail.com>
2021-07-27 23:43:22 +0530

    Added: Half_Day Wage Calculator

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -4,23 +4,20 @@ public class EmployeeWageCalculation
 {
 		 public static void main(String[] args) 
 	{
-		    	System.out.println(" Welcome to Employee Wage Computation ");
 		    	int Employee_Present = 1;
 		    	int WAGE_PER_HR = 20;
-		    	int FULL_DAY_HR = 8;
+		    	int FULL_DAY_HR = 16;
+		    	int HALF_DAY_HR = 8;
 		    	int Total_Wage_Per_Day;
-		    	double Employee_Check = Math.floor( Math.random() * 10 ) % 2;
+		    	int Employee_Check =(int)Math.floor( Math.random() * 10 ) % 2;
 		    	
-		    	System.err.println(" Let's first check  whether Employee is Present or Absent: -  ");
-		    	if ( Employee_Check == Employee_Present ) 
-		    	    {
-		    		System.out.println(" Employee is Present Today ");
+		    	if ( Employee_Check == Employee_Present ) {
 		    		Total_Wage_Per_Day = FULL_DAY_HR * WAGE_PER_HR;
-		    		System.out.println (" Employee wage per day is = " + Total_Wage_Per_Day );
+		    		System.out.println (" Employee wage Full day is = " + Total_Wage_Per_Day );
 		    		}
-		    			else {
-		    		    System.out.println(" Employee is Absent Today ");
-		    				 }
-	
-	}
+		    			else if (Employee_Check != Employee_Present) {
+		    					HALF_DAY_HR = HALF_DAY_HR * WAGE_PER_HR;
+		    					System.out.println(" Employee wage for Half Day is = " + HALF_DAY_HR);
+		    				 }	
+	 }
 }
